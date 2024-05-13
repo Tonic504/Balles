50bc02949d0cbf1b6221ed0d2b177aaa14c91943
Thomas <thomas.chaussende@gmail.com>
2024-05-12 22:07:15 -0400

    fin de l'animation, je vais maintenant faire toute la JavaDoc et l'entete de projet

--- src/main/java/com/example/ballerebond/AnimationController.java
+++ src/main/java/com/example/ballerebond/AnimationController.java
@@ -1,14 +1,22 @@
 package com.example.ballerebond;
 
+import javafx.animation.Animation;
+import javafx.animation.Interpolator;
+import javafx.animation.RotateTransition;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.image.Image;
 import javafx.scene.layout.StackPane;
+import javafx.scene.media.MediaPlayer;
 import javafx.scene.paint.ImagePattern;
 import javafx.scene.shape.Circle;
+import javafx.util.Duration;
 
+import java.io.File;
 import java.io.IOException;
+import java.nio.file.Paths;
 
+import javafx.scene.media.Media;
 public class AnimationController {
 
 
@@ -21,82 +29,138 @@ public class AnimationController {
     private final Image SPLATOONSIDEORDER = new Image("Images/SplatoonSideOrderFondCD.png");
     private final Image TETRISDS = new Image("Images/TetrisDSFondCD.png");
 
+    private MediaPlayer mediaPlayer;
+    private Media media;
+
+    private boolean peutChanger = true;
+
+
+
+    RotateTransition rotateTransition ;
+
+
 
     @FXML
-    private StackPane Stackdisque;
+    private StackPane stackDisque;
 
     @FXML
     private Circle disque;
 
     @FXML
     void arreter(ActionEvent event) {
-
+        if (rotateTransition != null) {rotateTransition.pause();}
+        if (mediaPlayer!=null){mediaPlayer.pause();}
     }
 
     @FXML
     void jouer(ActionEvent event) {
+        if (rotateTransition==null){
+        rotateTransition = new RotateTransition(Duration.seconds(3), stackDisque);
+        if (mediaPlayer!=null){mediaPlayer.stop();}
+        // Définir l'angle de départ et d'arrivée
+        rotateTransition.setFromAngle(0);
+        rotateTransition.setToAngle(360);
 
+        // Définir l'interpolateur pour un mouvement fluide
+        rotateTransition.setInterpolator(Interpolator.LINEAR);
+
+        // Répéter l'animation indéfiniment
+        rotateTransition.setCycleCount(Animation.INDEFINITE);
+        }
+        rotateTransition.play();
+
+        if(media==null){
+            media = new Media(getClass().getResource("Music/Gallery.mp3").toString());
+            peutChanger=false;
+        }
+        if(peutChanger){
+        mediaPlayer = new MediaPlayer(media);
+        mediaPlayer.play();
+        peutChanger = false;}
     }
+
 
     @FXML
     void jouerArkanoid(ActionEvent event) {
         changerImageCD(ARKANOID);
+        changerMusicCD("Music/HeatSink.mp3");
     }
 
     @FXML
     void jouerMLPIT(ActionEvent event) {
-changerImageCD(MLPIT);
+        changerImageCD(MLPIT);
+        changerMusicCD("Music/Boss Battle.mp3");
     }
 
     @FXML
     void jouerMSJO2008(ActionEvent event) {
-changerImageCD(MARIOETSONICJO);
+        changerImageCD(MARIOETSONICJO);
+        changerMusicCD("Music/TableTennis.mp3");
     }
 
     @FXML
     void jouerOctopath(ActionEvent event) {
-changerImageCD(OCTOPATHTRAVELERS);
+        changerImageCD(OCTOPATHTRAVELERS);
+        changerMusicCD("Music/OCTOPATHTRAVELER.mp3");
     }
 
     @FXML
     void jouerSonic(ActionEvent event) {
-changerImageCD(SONICCOLOURS);
+        changerImageCD(SONICCOLOURS);
+        changerMusicCD("Music/TerminalVelocity.mp3");
     }
 
     @FXML
     void jouerSplatoon(ActionEvent event) {
-changerImageCD(SPLATOONSIDEORDER);
+        changerImageCD(SPLATOONSIDEORDER);
+        changerMusicCD("Music/NewWorldOrder.mp3");
     }
 
     @FXML
     void jouerTetris(ActionEvent event) {
-changerImageCD(TETRISDS);
+        changerImageCD(TETRISDS);
+        changerMusicCD("Music/Tetris.mp3");
     }
 
     @FXML
     void jouerWiiSport(ActionEvent event) {
-changerImageCD(WIISPORT);
+        changerImageCD(WIISPORT);
+        changerMusicCD("Music/WiiSports.mp3");
     }
 
     @FXML
     void retourBallesRebondissantes(ActionEvent event) throws IOException {
         MainApplication.changerScene("balles-view.fxml","Balles Rebondissantes");
+        mediaPlayer.stop();
 
     }
 
     @FXML
     void retourMenu(ActionEvent event) throws IOException {
-    MainApplication.changerScene("main-view.fxml","Menu");
+        MainApplication.changerScene("main-view.fxml","Menu");
+        mediaPlayer.stop();
     }
 
     @FXML
     void retourObjet(ActionEvent event) throws IOException {
         MainApplication.changerSceneObjet("Objet 3D");
+        mediaPlayer.stop();
 
     }
 
     private void changerImageCD(Image cd){
         disque.setFill(new ImagePattern(cd));
     }
+
+    private void changerMusicCD(String chemin){
+        if(mediaPlayer!=null){
+        mediaPlayer.stop();}
+        if(rotateTransition!=null){rotateTransition.pause();}
+        String fullPath = getClass().getResource(chemin).toString();
+        media =new Media(getClass().getResource(chemin).toString());
+        peutChanger=true;
+    }
+
+
 
 }
--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,6 +1,7 @@
 module com.example.ballerebond {
     requires javafx.controls;
     requires javafx.fxml;
+    requires javafx.media;
 
 
     opens com.example.ballerebond to javafx.fxml;
